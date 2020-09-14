2f41decd066ca71acff4017be4b18d9064fe7b76
Kalaiselvan Ulaganathan <Kalaiselvan_Ulaganathan@epam.com>
2020-09-13 23:44:13 -0500

    gauth added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 'angularx-social-login';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/common/header/header.component';
 import { CourseDetailComponent } from './component/course-detail/course-detail.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { QuestionComponent } from './component/question/question.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule( {
   declarations: [
@@ -20,9 +21,25 @@ import { QuestionComponent } from './component/question/question.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '25175596994-338imgf6rt0dlt916ev29in8th6birlb.apps.googleusercontent.com'
+            )
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [ AppComponent ]
 } )
 export class AppModule { }
